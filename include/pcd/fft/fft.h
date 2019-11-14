d8b615223285cbd802e9855bf0cd39f13e92235a
Paul D'Angio <pcdangio@gmail.com>
2019-11-13 22:26:53 -0500

    initial implementation of recursive forward fft function

--- include/pcd/fft/fft.h
+++ include/pcd/fft/fft.h
@@ -7,20 +7,28 @@
 namespace pcd
 {
 
+template <class T>
 class fft
 {
 public:
     fft();
 
-    template <typename T>
     void forward(const std::vector<T>& input, std::vector<std::complex<T>>& output);
-
-    template <typename T>
     void inverse(const std::vector<std::complex<T>>& input, std::vector<T>& output);
 
 private:
-    template <typename T1, typename T2>
-    void fft_recurse(const std::vector<T1>& input, std::vector<T2>& output, uint32_t start, uint32_t step);
+    const std::vector<T>* real_input;
+    const std::vector<std::complex<T>>* complex_input;
+    uint32_t input_size;
+    uint32_t input_start;
+    uint32_t step_size;
+    std::vector<std::complex<T>> complex_output;
+    std::vector<T> real_output;
+    uint32_t output_start;
+
+    void fft_recurse();
+
+
 };
 }
 
--- src/fft.cpp
+++ src/fft.cpp
@@ -2,36 +2,72 @@
 
 using namespace pcd;
 
-fft::fft()
+template <class T>
+fft<T>::fft()
 {
 
 }
 
-template <typename T>
-void fft::forward(const std::vector<T>& input, std::vector<std::complex<T>>& output)
+template <class T>
+void fft<T>::forward(const std::vector<T>& input, std::vector<std::complex<T>>& output)
 {
 
 }
 
-template void fft::forward<float>(const std::vector<float>& input, std::vector<std::complex<float>>& output);
-template void fft::forward<double>(const std::vector<double>& input, std::vector<std::complex<double>>& output);
-
-template <typename T>
-void fft::inverse(const std::vector<std::complex<T>>& input, std::vector<T>& output)
+template <class T>
+void fft<T>::inverse(const std::vector<std::complex<T>>& input, std::vector<T>& output)
 {
 
 }
 
-template void fft::inverse<float>(const std::vector<std::complex<float>>& input, std::vector<float>& output);
-template void fft::inverse<double>(const std::vector<std::complex<double>>& input, std::vector<double>& output);
-
-template <typename T1, typename T2>
-void fft::fft_recurse(const std::vector<T1>& input, std::vector<T2>& output, uint32_t start, uint32_t step)
+template <class T>
+void fft<T>::fft_recurse()
 {
+    // Calculate size of the input vector using current starting index and step.
+    uint32_t n = (fft<T>::input_size - fft<T>::input_start - 1) / fft<T>::step + 1;
+
+    // Check size of the input vector.    
+    if(n == 2)
+    {
+        // Single even and single odd. Calculate FFT directly from input vector.
+        // Keep in mind that exp(-2*pi*k/N) = 1 when k = 0, so values are just even[start] + 1 * odd[start].
+        fft<T>::complex_output[fft<T>::output_start] = fft<T>::real_input[fft<T>::input_start] + fft<T>::real_input[fft<T>::input_start + fft<T>::step_size];
+        // This assumes that input is real only.  Complex conjugate of a real number is just the real number.
+        fft<T>::complex_output[fft<T>::output_start + 1] = fft<T>::complex_output[fft<T>::output_start];
+    }
+    else
+    {
+        // n_even and n_odd both > 1.  Recurse to get FFTs of the odd/even vectors.
+        // Precalculate n/2.
+        uint32_t n2 = n/2;
+
+        // Increment step size by factor of 2.
+        fft<T>::step_size *= 2;
+        // Run FFT on evens.
+        // Even branch always starts at current input_start, and writes to current output_start.
+        fft::fft_recurse();
+        // Odd branch always starts at current input_start + 1, and writes to output after even outputs (n/2)
+        fft<T>::input_start++;
+        fft<T>::output_start = n2;
+        fft::fft_recurse();
+        // Reverse changes to global, incrementally used input start/step
+        fft<T>::input_start--;
+        fft<T>::step_size /= 2;
+
+        // Calculate FFT from output vector.
+        // Iterate over k = 0 to N/2-1
+        for(uint32_t k = 0; k < n2; k++)
+        {
+            // output[k] = even[k] + exp(-2*pi*i*k/n) * odd[k]
+            fft<T>::complex_output[k] = fft<T>::complex_output[k] + std::exp<std::complex<T>>(-2 * M_PI * std::complex_literals::i * k / n) * fft<T>::complex_output[k + n2];
+            // output[k+n/2] = even[k] - w_nk * odd[k]
+            // This is the same as the complex conjugate of output[k]
+            // Using this method also allows us to overwrite complex_output[k] on the prior step and still calculate output[k+n/2]
+            fft<T>::complex_output[k + n2] = std::conj(fft<T>::complex_output[k]);
+        }
+    }
 
 }
 
-template void fft::fft_recurse<float, std::complex<float>>(const std::vector<float>& input, std::vector<std::complex<float>>& output, uint32_t start, uint32_t step);
-template void fft::fft_recurse<double, std::complex<double>>(const std::vector<double>& input, std::vector<std::complex<double>>& output, uint32_t start, uint32_t step);
-template void fft::fft_recurse<std::complex<float>, float>(const std::vector<std::complex<float>>& input, std::vector<float>& output, uint32_t start, uint32_t step);
-template void fft::fft_recurse<std::complex<double>, double>(const std::vector<std::complex<double>>& input, std::vector<double>& output, uint32_t start, uint32_t step);
+template class fft<float>;
+template class fft<double>;
